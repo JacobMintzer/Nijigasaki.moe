b27eb7db1a0712db1c069b1b63504b64816113f2
Jacob <jrmintz3@gmail.com>
2020-03-20 04:39:45 +0000

    modified some organization, did mostly nginx stuff

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ app.get('/',function(req,res){
 	res.sendFile(path.join(__dirname,'/public/home.html'));
 });
 
-var server = app.listen(8081, function(){
+var server = app.listen(8080, '10.128.0.6')
+/*function(){
     var port = server.address().port;
     console.log("Server started at http://localhost:%s", port);
-});
+});*/
